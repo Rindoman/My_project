e01a909e0ad06d3e5b605dc0dc6c6398a3ce2216
Mihail Yanenko <yanenko.web.dev@gmail.com>
2018-06-29 19:24:24 +0300

    change skillbar

--- app/js/anim.js
+++ app/js/anim.js
@@ -2,11 +2,11 @@ const wrapper = document.querySelector(".main-left");
 const text = document.querySelector(".run");
 const navbar = document.getElementById("topNavBar");
 const foto = document.querySelector(".main-right__img");
-const skill_90 = document.getElementById("skillbar__90");
-const skill_80 = document.getElementById("skillbar__80");
+const skill_90 = document.getElementById("skillbar__95");
+const skill_80 = document.getElementById("skillbar__90");
 const skill_75 = document.getElementById("skillbar__75");
-const skill_45 = document.getElementById("skillbar__45");
-const skill_40 = document.getElementById("skillbar__40");
+const skill_45 = document.getElementById("skillbar__50");
+const skill_40 = document.getElementById("skillbar__45");
 const skill_30 = document.getElementById("skillbar__30");
 
 const textCont = text.textContent;
@@ -28,11 +28,11 @@ function nav() {
     navbar.style.animationFillMode = 'forwards';
     navbar.style.animationTimingFunction = 'ease-out';
   } if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
-    skill_90.classList.add("skillbar__90");
-    skill_80.classList.add("skillbar__80");
+    skill_90.classList.add("skillbar__95");
+    skill_80.classList.add("skillbar__90");
     skill_75.classList.add("skillbar__75");
-    skill_45.classList.add("skillbar__45");
-    skill_40.classList.add("skillbar__40");
+    skill_45.classList.add("skillbar__50");
+    skill_40.classList.add("skillbar__45");
     skill_30.classList.add("skillbar__30");
   }  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     foto.style.animation = 'my-move 2s';
