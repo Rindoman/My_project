c7736cabd2b43f0a03f09a0c8784a1d3992b8f6e
Mihail Yanenko <yanenko.web.dev@gmail.com>
2019-04-07 15:26:18 +0300

    added internet shop to portfolio

--- app/js/anim.js
+++ app/js/anim.js
@@ -2,11 +2,11 @@ const wrapper = document.querySelector(".main-left");
 const text = document.querySelector(".run");
 const navbar = document.getElementById("topNavBar");
 const foto = document.querySelector(".main-right__img");
-const skill_90 = document.getElementById("skillbar__95");
-const skill_80 = document.getElementById("skillbar__90");
+const skill_95 = document.getElementById("skillbar__95");
+const skill_90 = document.getElementById("skillbar__90");
 const skill_75 = document.getElementById("skillbar__75");
-const skill_45 = document.getElementById("skillbar__50");
-const skill_40 = document.getElementById("skillbar__45");
+const skill_50 = document.getElementById("skillbar__50");
+const skill_45 = document.getElementById("skillbar__45");
 const skill_30 = document.getElementById("skillbar__30");
 
 const textCont = text.textContent;
@@ -23,30 +23,30 @@ function nav() {
     navbar.style.animationFillMode = 'forwards';
     navbar.style.animationTimingFunction = 'ease-in';
   } else if (document.body.scrollTop < 100 || document.documentElement.scrollTop < 100) {
-    navbar.style.position = 'fixed';
-    navbar.style.animation = 'move-up 1s';
-    navbar.style.animationFillMode = 'forwards';
-    navbar.style.animationTimingFunction = 'ease-out';
+      navbar.style.position = 'fixed';
+      navbar.style.animation = 'move-up 1s';
+      navbar.style.animationFillMode = 'forwards';
+      navbar.style.animationTimingFunction = 'ease-out';
   } if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
-    skill_90.classList.add("skillbar__95");
-    skill_80.classList.add("skillbar__90");
-    skill_75.classList.add("skillbar__75");
-    skill_45.classList.add("skillbar__50");
-    skill_40.classList.add("skillbar__45");
-    skill_30.classList.add("skillbar__30");
+      skill_95.classList.add("skillbar__95");
+      skill_90.classList.add("skillbar__90");
+      skill_75.classList.add("skillbar__75");
+      skill_50.classList.add("skillbar__50");
+      skill_45.classList.add("skillbar__45");
+      skill_30.classList.add("skillbar__30");
   }  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    foto.style.animation = 'my-move 2s';
-    foto.style.display = 'block';
-    while ( i < textCont.length) {
-      (function(i) {
-        setTimeout(function() {
-          let texts = document.createTextNode(textCont[i])
-          let span = document.createElement('span');
-          span.appendChild(texts);
-          span.classList.add("wave");
-          wrapper.appendChild(span);
-        }, 50 * i);
-      }(i));
+      foto.style.animation = 'my-move 2s';
+      foto.style.display = 'block';
+      while ( i < textCont.length) {
+        (function(i) {
+          setTimeout(function() {
+            let waveText = document.createTextNode(textCont[i])
+            let span = document.createElement('span');
+            span.appendChild(waveText);
+            span.classList.add("wave");
+            wrapper.appendChild(span);
+          }, 50 * i);
+        }(i));
       i++;
     }
     clearTimeout();
--- app/js/mobile_script.js
+++ app/js/mobile_script.js
@@ -1,14 +1,14 @@
+const skill_95 = document.getElementById("skillbar__95");
 const skill_90 = document.getElementById("skillbar__90");
-const skill_80 = document.getElementById("skillbar__80");
 const skill_75 = document.getElementById("skillbar__75");
+const skill_50 = document.getElementById("skillbar__50");
 const skill_45 = document.getElementById("skillbar__45");
-const skill_40 = document.getElementById("skillbar__40");
 const skill_30 = document.getElementById("skillbar__30");
 
 let message = "Hey! I'm a web developer from Ukraine. I deeply interested in programming field and I constantly improve my professional level. Currently I work remotely, but I'm willing to work at the dinamic company for further improving my skills.";
 let msgCount = 0;
 let timer;
-let messageLabel = document.getElementById("run");
+const messageLabel = document.getElementById("run");
 
 function textFunc() {
    messageLabel.innerHTML = message.substring(0, msgCount);   
@@ -24,11 +24,11 @@ window.onscroll = function() {nav()};
 
 function nav() {
 if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    skill_95.classList.add("skillbar__95");
     skill_90.classList.add("skillbar__90");
-    skill_80.classList.add("skillbar__80");
     skill_75.classList.add("skillbar__75");
+    skill_50.classList.add("skillbar__50");
     skill_45.classList.add("skillbar__45");
-    skill_40.classList.add("skillbar__40");
     skill_30.classList.add("skillbar__30");
   }
 }
